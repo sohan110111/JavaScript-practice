f53937ab36b6afcc53540c96bf9811a2c0374ddd
sohan110111 <sohan110111@gmail.com>
2020-03-01 11:11:05 +0600

    while loop and debug practice

---
+++ while.js
@@ -0,0 +1,12 @@
+var num = 0; //num means number
+
+while (num < 15) {
+    console.log(num);
+    num++;  // num = num + 1;
+}
+
+// given below the code print only '15'
+while (num <= 15) {
+    console.log(num);
+    num++;  // num = num + 1;
+}
