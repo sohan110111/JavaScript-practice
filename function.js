6ea5da9f0b0d749a20abb3e221f2a4e9faac8f5d
sohan110111 <sohan110111@gmail.com>
2020-03-01 12:29:43 +0600

    function declaration and call function

---
+++ function.js
@@ -0,0 +1,17 @@
+// function declaration
+function sayLoveYou() {  
+    console.log("Hey, where are you?");
+    console.log("What are you doing now.");
+}
+
+// call function 
+sayLoveYou();
+
+function callFriends() {  
+    console.log("Hey, where are you?");
+    console.log("What are you doing now.");
+    console.log("Come here.");
+}
+var place = "TSC";
+console.log(place);
+callFriends();
